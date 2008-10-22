9326bdbf3a809de43ab086bf34ab0259b05217ed
Peter Burns <peter@metaweb.com>
2008-10-22 11:28:14 -0700

    Added documentation and stubs for the system calls we'll need to implement

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -4,6 +4,77 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+/* Terminates Pintos by calling power_off() (declared in "threads/init.h"). 
+ This should be seldom used, because you lose some information about possible
+ deadlock situations, etc.  */
+static void halt (void){}
+
+/* Terminates the current user program, returning status to the kernel. If 
+  the process's parent waits for it (see below), this is the status that 
+  will be returned. Conventionally, a status of 0 indicates success and 
+  nonzero values indicate errors. */
+static void exit (int status) {}
+
+/* Runs the executable whose name is given in cmd_line, passing any 
+  given arguments, and returns the new process's program id (pid).
+  Must return pid -1, which otherwise should not be a valid pid, 
+  if the program cannot load or run for any reason. */
+static int exec (const char *cmd_line) {}
+
+/* If process pid is still alive, waits until it dies. Then, returns
+ the status that pid passed to exit, or -1 if pid was terminated by 
+ the kernel (e.g. killed due to an exception). If pid does not refer 
+ to a child of the calling thread, or if wait has already been 
+ successfully called for the given pid, returns -1 immediately, 
+ without waiting. */
+static int wait (int pid){
+  //initially an infinite loop
+  while(true);
+}
+
+/* Creates a new file called file initially initial_size bytes in size.
+ Returns true if successful, false otherwise. */
+static bool create (const char *file, unsigned initial_size){
+  return false;
+}
+
+/* Deletes the file called file. Returns true if successful, false otherwise.*/ 
+static bool remove (const char *file){
+  return false;
+}
+
+/* Opens the file called file. Returns a nonnegative integer handle called a 
+  "file descriptor" (fd), or -1 if the file could not be opened. */
+static int open (const char *file){}
+
+/* Returns the size, in bytes, of the file open as fd. */
+static int filesize (int fd){}
+
+/* Reads size bytes from the file open as fd into buffer. Returns the number of
+ bytes actually read (0 at end of file), or -1 if the file could not be read 
+ (due to a condition other than end of file). Fd 0 reads from the keyboard 
+ using input_getc(). */
+static int read (int fd, void *buffer, unsigned size){}
+
+/* Writes size bytes from buffer to the open file fd. Returns the number of 
+  bytes actually written, or -1 if the file could not be written. */
+static int write (int fd, const void *buffer, unsigned size){}
+
+/* Changes the next byte to be read or written in open file fd to position, 
+  expressed in bytes from the beginning of the file. (Thus, a position of 
+  0 is the file's start.) */
+static void seek (int fd, unsigned position){}
+
+/* Returns the position of the next byte to be read or written in open file 
+  fd, expressed in bytes from the beginning of the file. */
+static unsigned tell (int fd){}
+
+/* Closes file descriptor fd. Exiting or terminating a process implicitly 
+  closes all its open file descriptors, as if by calling this function 
+  for each one. */
+static void close (int fd){}
+
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -16,5 +87,11 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  
   thread_exit ();
 }
+
+
+
+
+
