8b5fc64a2897d17eb719349e4239b92aa7f30c74
Lihuan Xie <rikutse@localhost.localdomain>
2008-11-01 16:19:50 -0700

    beta on implementing validate memory address.

--- userprog/exception.c
+++ userprog/exception.c
@@ -148,14 +148,6 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
   switch (f->cs)
 	{
 	case SEL_KCSEG:
@@ -163,6 +155,14 @@ page_fault (struct intr_frame *f)
 		f->eax = -1;
 		break;
 	default:
+		  /* To implement virtual memory, delete the rest of the function
+		     body, and replace it with code that brings in the page to
+		     which fault_addr refers. */
+		  printf ("Page fault at %p: %s error %s page in %s context.\n",
+		          fault_addr,
+		          not_present ? "not present" : "rights violation",
+		          write ? "writing" : "reading",
+		          user ? "user" : "kernel");
 		  kill (f);	
 	}
 }
